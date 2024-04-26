f7111dff43a6d564f43b4de5725ff50d68baa063
Yashwanth-PS <yashwanth.ps.143@gmail.com>
2024-04-26 22:59:04 +0530

    GoogleMeetLLD - Created the Meeting Response DTO

---
+++ src/main/java/com/googlemeet/dto/MeetingResponseDTO.java
@@ -0,0 +1,11 @@
+package com.googlemeet.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MeetingResponseDTO {
+    private int meetingId;
+    private String meetingPassword;
+}
